d2ba375fba109cc146efab4ae491651f433dfef2
catieterrey <caterrey@edu.uwaterloo.ca>
2020-03-01 20:35:37 -0500

    a

---
+++ N_Body_Problem/N_Body_Problem/Particle.cpp
@@ -0,0 +1,17 @@
+//#include "Particle.h"
+//
+//public std::vector<double> Particle::Position;
+//public std::vector<double> Particle::Momentum;
+//public double Particle::Mass;
+//
+//Particle::Particle(std::vector<double> position, std::vector<double> momentum, double mass) {
+//	Particle::Position = position;
+//	Particle::Momentum = momentum;
+//	Particle::Mass = mass;
+//}
+//
+//
+//Particle::~Particle()
+//{
+//}
+
